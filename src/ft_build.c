76999e2778f49786ab4adf25bc3f313a3df7f9cb
Akinfemi Akin-Aluko <aakin-al@e1z2r8p15.42.us.org>
2017-06-19 22:50:13 -0700

    norm build

--- src/ft_build.c
+++ src/ft_build.c
@@ -6,48 +6,49 @@
 /*   By: aakin-al <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/26 09:22:25 by aakin-al          #+#    #+#             */
-/*   Updated: 2017/05/26 10:31:09 by aakin-al         ###   ########.fr       */
+/*   Updated: 2017/06/19 22:49:37 by aakin-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_printf.h>
 
-void	ft_build(va_list ap, t_format *p, t_output **output) {
-    char *res;
-    t_output *out;
-    int len;
-    char ch;
+void		ft_build(va_list ap, t_format *p, t_output **output)
+{
+	char		*res;
+	t_output	*out;
+	int			len;
+	char		ch;
 
-    out = *output;
-    res = handle_conv(ap, p->conv, p);
-    if (p->conv == 's' && p->precision == 0 && p->period == 1)
-        res = "";
-    len = ft_strlen(res);
-    ch = ' ';
-    handle_space(&p, output);
-    handle_padding(&p, output, res, len);
-    handle_precision(&p, output, len);
-    handle_res(&p, output, res, len);
-    handle_alignment(&p, output);
+	out = *output;
+	res = handle_conv(ap, p->conv, p);
+	if (p->conv == 's' && p->precision == 0 && p->period == 1)
+		res = "";
+	len = ft_strlen(res);
+	ch = ' ';
+	handle_space(&p, output);
+	handle_padding(&p, output, res, len);
+	handle_precision(&p, output, len);
+	handle_res(&p, output, res, len);
+	handle_alignment(&p, output);
 }
 
 void		ft_add(char *str, t_output **output, int signal)
 {
-    t_output *out;
-    char        *temp;
+	t_output	*out;
+	char		*temp;
 
-    out = *output;
-    if (signal == 3 || signal == 1)
-        out->len += ft_strlen(str);
-    else if (signal == 2)
-    {
-        *str = 127;
-        out->len += 1;
-    }
-    temp = out->res;
-    if (ft_strlen(temp))
-        free(temp);
-    out->res = ft_strjoin(out->res, str);
-    if (signal == 3 || signal == 4)
-        free (str);
+	out = *output;
+	if (signal == 3 || signal == 1)
+		out->len += ft_strlen(str);
+	else if (signal == 2)
+	{
+		*str = 127;
+		out->len += 1;
+	}
+	temp = out->res;
+	if (ft_strlen(temp))
+		free(temp);
+	out->res = ft_strjoin(out->res, str);
+	if (signal == 3 || signal == 4)
+		free(str);
 }
